dfdcbcd2a7d88b3c4dc2a5e6a31cec2ee4061be9
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 06:38:16 -0400

    Update UserRatings.js

--- models/UserRatings.js
+++ models/UserRatings.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 
 const userRatingsSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  ratings: [
-    {
-      systemName: String,
-      score: Number,
-      reason: String
-    }
-  ]
+  system: { type: String, required: true },
+  score: { type: Number, required: true },
+  reason: { type: String, default: 'No reason provided.' }
 });
 
 module.exports = mongoose.model('UserRatings', userRatingsSchema);
