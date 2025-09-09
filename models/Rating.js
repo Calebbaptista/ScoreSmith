365e29ec8617b88e5bd8f6765d96f17f929ea40e
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 03:30:38 -0400

    Create Rating.js

---
+++ models/Rating.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const ratingSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  threshold: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Rating', ratingSchema);
