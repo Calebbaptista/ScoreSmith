248713fe79f5a2ece3c0dc4cccf136f9d04e2dc5
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 06:35:56 -0400

    Delete models/MainRole.js

--- models/MainRole.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const mainRoleSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  roleId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('MainRole', mainRoleSchema);
