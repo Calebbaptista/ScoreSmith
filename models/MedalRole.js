584891b844817037e51e29d07bdb9642c31c0676
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 06:35:24 -0400

    Delete models/MedalRole.js

--- models/MedalRole.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const medalRoleSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  roleId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('MedalRole', medalRoleSchema);
