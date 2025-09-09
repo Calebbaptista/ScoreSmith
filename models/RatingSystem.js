a14b6ab5742551945d5cb5a15649179328aa5ff7
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 06:38:06 -0400

    Update RatingSystem.js

--- models/RatingSystem.js
+++ models/RatingSystem.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const ratingSystemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: String,
-  requiredRoleIds: [{ type: String }] // Optional role restrictions
+  description: { type: String, default: '' }
 });
-
-ratingSystemSchema.index({ name: 1 }, { unique: true });
 
 module.exports = mongoose.model('RatingSystem', ratingSystemSchema);
