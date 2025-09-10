5e3c5cae9e680b683da88dccab556c4f45749674
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:58:00 -0400

    Update addpoints.js

--- commands/points/addpoints.js
+++ commands/points/addpoints.js
@@ -1,30 +1,26 @@
-const UserPoints = require('../../models/UserPoints');
-const PointLimit = require('../../models/PointLimit');
-const PointType = require('../../models/PointType');
+const { SlashCommandBuilder } = require('discord.js');
+const Point = require('../models/Point');
 
-module.exports = async (interaction) => {
-  const guildId = interaction.guild.id;
-  const user = interaction.options.getUser('user');
-  const type = interaction.options.getString('type');
-  const amount = interaction.options.getInteger('amount');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('addpoint')
+    .setDescription('Add a point to a user')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to give a point to')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('type')
+        .setDescription('Point type')
+        .setRequired(true)
+        .setAutocomplete(true)),
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    const type = interaction.options.getString('type');
+    const guildId = interaction.guild.id;
 
-  const limit = await PointLimit.findOne({ guildId });
-  if (limit && amount > limit.maxAmount) {
-    return interaction.reply({ content: `⛔ Max allowed is ${limit.maxAmount}.`, ephemeral: true });
+    await Point.create({ userId: user.id, guildId, type, timestamp: new Date() });
+
+    await interaction.reply({ content: `✅ Point of type **${type}** added to <@${user.id}>.`, flags: 64 });
   }
-
-  const typeExists = await PointType.findOne({ guildId, name: type });
-  if (!typeExists) {
-    return interaction.reply({ content: '⚠️ Invalid point type.', ephemeral: true });
-  }
-
-  const current = await UserPoints.findOne({ userId: user.id, guildId, type });
-  if (current) {
-    current.amount += amount;
-    await current.save();
-  } else {
-    await UserPoints.create({ userId: user.id, guildId, type, amount });
-  }
-
-  await interaction.reply({ content: `✅ Added ${amount} ${type} to <@${user.id}>.`, ephemeral: true });
 };
