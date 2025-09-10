dc72b0084be47684275f8c322ea2ab84d0e72685
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:15:31 -0400

    Delete models/LogChannel.js

--- models/LogChannel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const LogChannelSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  channelId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('LogChannel', LogChannelSchema);
