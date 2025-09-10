60500ba18b0339f23516dfa5810a90be065c3dad
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:31:29 -0400

    Update removepointtype.js

--- commands/points/removepointtype.js
+++ commands/points/removepointtype.js
@@ -1,1 +1,13 @@
+const PointType = require('../../models/PointType');
 
+module.exports = async (interaction) => {
+  const guildId = interaction.guild.id;
+  const name = interaction.options.getString('name');
+
+  const deleted = await PointType.deleteOne({ guildId, name });
+  if (deleted.deletedCount === 0) {
+    return interaction.reply({ content: '⚠️ No such point type found.', ephemeral: true });
+  }
+
+  await interaction.reply({ content: `🗑️ Point type **${name}** removed.`, ephemeral: true });
+};
