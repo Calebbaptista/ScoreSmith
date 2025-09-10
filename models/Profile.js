73db1de122bd3731ed476ee1b4ac1f0da7f420a5
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-10 02:45:07 -0400

    Update Profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const profileSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  guildId: { type: String, required: true },
-  title: { type: String },
-  bio: { type: String },
-  joinDate: { type: Date, default: Date.now }
+  userId: String,
+  guildId: String,
+  title: String,
+  joinDate: Date
 });
 
 module.exports = mongoose.model('Profile', profileSchema);
