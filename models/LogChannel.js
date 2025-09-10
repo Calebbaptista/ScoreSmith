16d164396f4d58679ca2666f36175164b499e431
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:38:40 -0400

    Update LogChannel.js

--- models/LogChannel.js
+++ models/LogChannel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const logChannelSchema = new mongoose.Schema({
+const LogChannelSchema = new mongoose.Schema({
   guildId: { type: String, required: true },
   channelId: { type: String, required: true }
 });
 
-module.exports = mongoose.model('LogChannel', logChannelSchema);
+module.exports = mongoose.model('LogChannel', LogChannelSchema);
