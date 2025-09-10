1899d430d5036d22e4da3475539bfe10aa631970
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:04:07 -0400

    Update removepoints.js

--- commands/points/removepoints.js
+++ commands/points/removepoints.js
@@ -1,30 +1,31 @@
 const { SlashCommandBuilder } = require('discord.js');
-const Point = require('../models/Point');
+const Point = require('../../models/Point');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('removepoint')
-    .setDescription('Remove a point from a user')
+    .setName('removepoints')
+    .setDescription('Remove multiple points from a user')
     .addUserOption(option =>
       option.setName('user')
-        .setDescription('User to remove a point from')
+        .setDescription('User to remove points from')
         .setRequired(true))
     .addStringOption(option =>
       option.setName('type')
         .setDescription('Point type')
         .setRequired(true)
-        .setAutocomplete(true)),
+        .setAutocomplete(true))
+    .addIntegerOption(option =>
+      option.setName('amount')
+        .setDescription('Number of points to remove')
+        .setRequired(true)),
   async execute(interaction) {
     const user = interaction.options.getUser('user');
     const type = interaction.options.getString('type');
+    const amount = interaction.options.getInteger('amount');
     const guildId = interaction.guild.id;
 
-    const result = await Point.findOneAndDelete({ userId: user.id, guildId, type });
+    const result = await Point.deleteMany({ userId: user.id, guildId, type }).limit(amount);
 
-    if (result) {
-      await interaction.reply({ content: `🗑️ Removed one **${type}** point from <@${user.id}>.`, flags: 64 });
-    } else {
-      await interaction.reply({ content: `⚠️ No point of type **${type}** found for <@${user.id}>.`, flags: 64 });
-    }
+    await interaction.reply({ content: `🗑️ Removed up to **${amount}** ${type} point(s) from <@${user.id}>.`, flags: 64 });
   }
 };
