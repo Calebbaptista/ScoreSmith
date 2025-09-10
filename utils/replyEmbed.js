9497de92c3e727702951ae3aff9fa01279548e22
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:39:11 -0400

    Update replyEmbed.js

--- utils/replyEmbed.js
+++ utils/replyEmbed.js
@@ -1,1 +1,15 @@
-
+module.exports = (title, description, interaction, color = 0x6A0DAD) => ({
+  embeds: [{
+    title,
+    description,
+    color,
+    timestamp: new Date().toISOString(),
+    thumbnail: {
+      url: interaction.guild.iconURL({ extension: 'png', size: 128 }) || ''
+    },
+    footer: {
+      text: `By ${interaction.user.tag}`,
+      icon_url: interaction.user.displayAvatarURL({ extension: 'png', size: 64 })
+    }
+  }]
+});
