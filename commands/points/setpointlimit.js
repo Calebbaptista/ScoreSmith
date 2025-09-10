f18e0bb854f348cae79e02406236471845bb9378
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:32:34 -0400

    Update setpointlimit.js

--- commands/points/setpointlimit.js
+++ commands/points/setpointlimit.js
@@ -1,1 +1,14 @@
+const PointLimit = require('../../models/PointLimit');
 
+module.exports = async (interaction) => {
+  const guildId = interaction.guild.id;
+  const maxAmount = interaction.options.getInteger('max');
+
+  await PointLimit.findOneAndUpdate(
+    { guildId },
+    { maxAmount },
+    { upsert: true }
+  );
+
+  await interaction.reply({ content: `✅ Point transfer limit set to **${maxAmount}**.`, ephemeral: true });
+};
