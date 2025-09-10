1b72d359d1a55a59679b48a1b78a9991101cf0f2
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:34:37 -0400

    Update removeratingtype.js

--- commands/ratings/removeratingtype.js
+++ commands/ratings/removeratingtype.js
@@ -1,1 +1,12 @@
+const RatingSystem = require('../../models/RatingSystem');
 
+module.exports = async (interaction) => {
+  const name = interaction.options.getString('name');
+
+  const deleted = await RatingSystem.deleteOne({ name });
+  if (deleted.deletedCount === 0) {
+    return interaction.reply({ content: '⚠️ No such rating system found.', ephemeral: true });
+  }
+
+  await interaction.reply({ content: `🗑️ Rating system **${name}** removed.`, ephemeral: true });
+};
