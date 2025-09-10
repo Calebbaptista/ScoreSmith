5cfdee7cfaf66e00c0a648f81a987d1397c574ee
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:15:15 -0400

    Delete models/PointAccess.js

--- models/PointAccess.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PointAccessSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  type: { type: String, required: true },
-  allowedRoles: { type: [String], default: [] }
-});
-
-module.exports = mongoose.model('PointAccess', PointAccessSchema);
