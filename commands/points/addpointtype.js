08e1bb94bfe19fd9ac5c9c4062600816310cf084
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:31:16 -0400

    Update addpointtype.js

--- commands/points/addpointtype.js
+++ commands/points/addpointtype.js
@@ -1,1 +1,14 @@
+const PointType = require('../../models/PointType');
 
+module.exports = async (interaction) => {
+  const guildId = interaction.guild.id;
+  const name = interaction.options.getString('name');
+
+  const existing = await PointType.findOne({ guildId, name });
+  if (existing) {
+    return interaction.reply({ content: '⚠️ That point type already exists.', ephemeral: true });
+  }
+
+  await PointType.create({ guildId, name });
+  await interaction.reply({ content: `✅ Point type **${name}** created.`, ephemeral: true });
+};
