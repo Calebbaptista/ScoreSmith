b7ae6a5a40920ef61e58dcf4a255598559519aa0
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:02:42 -0400

    Update removeratingtype.js

--- commands/ratings/removeratingtype.js
+++ commands/ratings/removeratingtype.js
@@ -1,12 +1,24 @@
-const RatingSystem = require('../../models/RatingSystem');
+const { SlashCommandBuilder } = require('discord.js');
+const RatingType = require('../../models/RatingType');
 
-module.exports = async (interaction) => {
-  const name = interaction.options.getString('name');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('removeratingtype')
+    .setDescription('Remove an existing rating type')
+    .addStringOption(option =>
+      option.setName('name')
+        .setDescription('Name of the rating type to remove')
+        .setRequired(true)),
+  async execute(interaction) {
+    const name = interaction.options.getString('name');
+    const guildId = interaction.guild.id;
 
-  const deleted = await RatingSystem.deleteOne({ name });
-  if (deleted.deletedCount === 0) {
-    return interaction.reply({ content: '⚠️ No such rating system found.', ephemeral: true });
+    const result = await RatingType.findOneAndDelete({ guildId, name });
+
+    if (result) {
+      await interaction.reply({ content: `🗑️ Rating type **${name}** removed.`, flags: 64 });
+    } else {
+      await interaction.reply({ content: `⚠️ Rating type **${name}** not found.`, flags: 64 });
+    }
   }
-
-  await interaction.reply({ content: `🗑️ Rating system **${name}** removed.`, ephemeral: true });
 };
