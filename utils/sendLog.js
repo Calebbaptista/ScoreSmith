bebe55565485909e72e90080293b63c1296ba4ac
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:39:27 -0400

    Update sendLog.js

--- utils/sendLog.js
+++ utils/sendLog.js
@@ -1,1 +1,16 @@
+const LogChannel = require('../models/LogChannel');
+const { client } = require('../index'); // or pass client in if needed
 
+module.exports = async (guildId, embed) => {
+  try {
+    const config = await LogChannel.findOne({ guildId });
+    if (!config) return;
+
+    const channel = await client.channels.fetch(config.channelId);
+    if (!channel) return;
+
+    await channel.send(embed);
+  } catch (err) {
+    console.error('❌ Logging error:', err);
+  }
+};
