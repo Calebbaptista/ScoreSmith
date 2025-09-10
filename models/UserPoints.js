a916b8764501bd04739defc63dbe2bb89f0c9db7
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:37:42 -0400

    Update UserPoints.js

--- models/UserPoints.js
+++ models/UserPoints.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const userPointsSchema = new mongoose.Schema({
+const UserPointsSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   guildId: { type: String, required: true },
   type: { type: String, required: true },
   amount: { type: Number, default: 0 }
 });
 
-module.exports = mongoose.model('UserPoints', userPointsSchema);
+module.exports = mongoose.model('UserPoints', UserPointsSchema);
