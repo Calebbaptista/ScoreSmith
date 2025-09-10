dd0ec0b6e25f4300ade2299185e71c70eb98a6a0
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:15:09 -0400

    Delete models/PointLimit.js

--- models/PointLimit.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const PointLimitSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  maxAmount: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('PointLimit', PointLimitSchema);
