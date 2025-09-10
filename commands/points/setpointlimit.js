433f66490dfc9d3fb2706b0bfe7c7cc3e931b852
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:04:53 -0400

    Update setpointlimit.js

--- commands/points/setpointlimit.js
+++ commands/points/setpointlimit.js
@@ -1,14 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js');
 const PointLimit = require('../../models/PointLimit');
 
-module.exports = async (interaction) => {
-  const guildId = interaction.guild.id;
-  const maxAmount = interaction.options.getInteger('max');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('setpointlimit')
+    .setDescription('Set a daily point limit for a type')
+    .addStringOption(option =>
+      option.setName('type')
+        .setDescription('Point type')
+        .setRequired(true))
+    .addIntegerOption(option =>
+      option.setName('limit')
+        .setDescription('Maximum points per day')
+        .setRequired(true)),
+  async execute(interaction) {
+    const type = interaction.options.getString('type');
+    const limit = interaction.options.getInteger('limit');
+    const guildId = interaction.guild.id;
 
-  await PointLimit.findOneAndUpdate(
-    { guildId },
-    { maxAmount },
-    { upsert: true }
-  );
+    await PointLimit.findOneAndUpdate(
+      { guildId, type },
+      { $set: { limit } },
+      { upsert: true }
+    );
 
-  await interaction.reply({ content: `✅ Point transfer limit set to **${maxAmount}**.`, ephemeral: true });
+    await interaction.reply({ content: `📏 Daily limit for **${type}** set to **${limit}**.`, flags: 64 });
+  }
 };
