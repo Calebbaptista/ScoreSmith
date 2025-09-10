6c6060dad16ad96a7a393b59b64d00d13ddf71f5
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:35:50 -0400

    Update viewpointlimit.js

--- commands/system/viewpointlimit.js
+++ commands/system/viewpointlimit.js
@@ -1,1 +1,18 @@
+const PointLimit = require('../../models/PointLimit');
 
+module.exports = async (interaction) => {
+  const guildId = interaction.guild.id;
+  const limit = await PointLimit.findOne({ guildId });
+
+  if (!limit) {
+    return interaction.reply({
+      content: '⚠️ No point limit has been set.',
+      ephemeral: true
+    });
+  }
+
+  await interaction.reply({
+    content: `📏 Current point transfer limit is **${limit.maxAmount}**.`,
+    ephemeral: true
+  });
+};
