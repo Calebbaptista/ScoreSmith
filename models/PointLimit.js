09438154743db78108d0e8cf77a4b17f218f83f8
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-10 03:08:10 -0400

    Update PointLimit.js

--- models/PointLimit.js
+++ models/PointLimit.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const pointLimitSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  type: { type: String, required: true },
-  limit: { type: Number, required: true }
+  guildId: String,
+  limit: Number
 });
 
 module.exports = mongoose.model('PointLimit', pointLimitSchema);
