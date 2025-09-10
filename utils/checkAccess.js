65a917e5734c4a551ed34cd709f79287560d4b9f
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:39:39 -0400

    Update checkAccess.js

--- utils/checkAccess.js
+++ utils/checkAccess.js
@@ -1,1 +1,10 @@
+const PointAccess = require('../models/PointAccess');
 
+module.exports = async (interaction, type) => {
+  const guildId = interaction.guild.id;
+  const access = await PointAccess.findOne({ guildId, type });
+  if (!access) return true;
+
+  const memberRoles = interaction.member.roles.cache.map(r => r.id);
+  return access.allowedRoles.some(roleId => memberRoles.includes(roleId));
+};
