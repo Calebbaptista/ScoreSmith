05d462ae72eeb29fe28c7377309a89cccfb1f9ff
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-10 03:06:45 -0400

    Update addpoints.js

--- commands/points/addpoints.js
+++ commands/points/addpoints.js
@@ -1,13 +1,14 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Point = require('../../models/Point');
+const PointLimit = require('../../models/PointLimit');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('addpoints')
-    .setDescription('Add points to a user')
-    .addUserOption(option =>
-      option.setName('user')
-        .setDescription('User to award points to')
+    .setDescription('Add points to one or more users')
+    .addMentionableOption(option =>
+      option.setName('targets')
+        .setDescription('User(s) or role to award points to')
         .setRequired(true)
     )
     .addStringOption(option =>
@@ -23,21 +24,41 @@ module.exports = {
     )
     .addIntegerOption(option =>
       option.setName('amount')
-        .setDescription('Number of points to add')
+        .setDescription('Points to add per user')
         .setRequired(true)
     ),
   async execute(interaction) {
-    const user = interaction.options.getUser('user');
+    const target = interaction.options.getMentionable('targets');
     const type = interaction.options.getString('type');
     const amount = interaction.options.getInteger('amount');
     const guildId = interaction.guild.id;
 
-    for (let i = 0; i < amount; i++) {
-      await Point.create({ userId: user.id, guildId, type });
+    const config = await PointLimit.findOne({ guildId });
+    const limit = config?.limit || 10;
+
+    if (amount > limit) {
+      await interaction.reply({
+        content: `⚠️ You can only add up to ${limit} points at a time.`,
+        flags: 1 << 6
+      });
+      return;
+    }
+
+    let users = [];
+    if (target.user) {
+      users = [target.user];
+    } else if (target.members) {
+      users = Array.from(target.members.values());
+    }
+
+    for (const user of users) {
+      for (let i = 0; i < amount; i++) {
+        await Point.create({ userId: user.id, guildId, type });
+      }
     }
 
     await interaction.reply({
-      content: `✅ Added ${amount} **${type}** point(s) to ${user.username}.`,
+      content: `✅ Added ${amount} **${type}** point(s) to ${users.map(u => u.username).join(', ')}.`,
       flags: 1 << 6
     });
   }
