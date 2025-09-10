bb3c2106666fd5e7a0539da2d5f11fc4ee2937b9
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:35:31 -0400

    Update setlogchannel.js

--- commands/system/setlogchannel.js
+++ commands/system/setlogchannel.js
@@ -1,1 +1,17 @@
+const LogChannel = require('../../models/LogChannel');
 
+module.exports = async (interaction) => {
+  const guildId = interaction.guild.id;
+  const channel = interaction.options.getChannel('channel');
+
+  await LogChannel.findOneAndUpdate(
+    { guildId },
+    { channelId: channel.id },
+    { upsert: true }
+  );
+
+  await interaction.reply({
+    content: `✅ Log channel set to <#${channel.id}>.`,
+    ephemeral: true
+  });
+};
