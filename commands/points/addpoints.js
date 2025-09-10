2a11853ec848739e16c00cbf7af75178a7dad912
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:03:53 -0400

    Update addpoints.js

--- commands/points/addpoints.js
+++ commands/points/addpoints.js
@@ -1,26 +1,38 @@
 const { SlashCommandBuilder } = require('discord.js');
-const Point = require('../models/Point');
+const Point = require('../../models/Point');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('addpoint')
-    .setDescription('Add a point to a user')
+    .setName('addpoints')
+    .setDescription('Add multiple points to a user')
     .addUserOption(option =>
       option.setName('user')
-        .setDescription('User to give a point to')
+        .setDescription('User to give points to')
         .setRequired(true))
     .addStringOption(option =>
       option.setName('type')
         .setDescription('Point type')
         .setRequired(true)
-        .setAutocomplete(true)),
+        .setAutocomplete(true))
+    .addIntegerOption(option =>
+      option.setName('amount')
+        .setDescription('Number of points to add')
+        .setRequired(true)),
   async execute(interaction) {
     const user = interaction.options.getUser('user');
     const type = interaction.options.getString('type');
+    const amount = interaction.options.getInteger('amount');
     const guildId = interaction.guild.id;
 
-    await Point.create({ userId: user.id, guildId, type, timestamp: new Date() });
+    const points = Array.from({ length: amount }, () => ({
+      userId: user.id,
+      guildId,
+      type,
+      timestamp: new Date()
+    }));
 
-    await interaction.reply({ content: `✅ Point of type **${type}** added to <@${user.id}>.`, flags: 64 });
+    await Point.insertMany(points);
+
+    await interaction.reply({ content: `✅ Added **${amount}** ${type} point(s) to <@${user.id}>.`, flags: 64 });
   }
 };
