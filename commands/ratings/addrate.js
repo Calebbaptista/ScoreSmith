a2b2b3f00000ed1b5dfc42bbfc538f5ed0394ce1
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 20:34:48 -0400

    Update addrate.js

--- commands/ratings/addrate.js
+++ commands/ratings/addrate.js
@@ -1,1 +1,25 @@
+const UserRatings = require('../../models/UserRatings');
+const RatingSystem = require('../../models/RatingSystem');
 
+module.exports = async (interaction) => {
+  const user = interaction.options.getUser('user');
+  const system = interaction.options.getString('system');
+  const score = interaction.options.getInteger('score');
+  const reason = interaction.options.getString('reason') || '';
+
+  const systemExists = await RatingSystem.findOne({ name: system });
+  if (!systemExists) {
+    return interaction.reply({ content: '⚠️ Invalid rating system.', ephemeral: true });
+  }
+
+  const existing = await UserRatings.findOne({ userId: user.id, system });
+  if (existing) {
+    existing.score = score;
+    existing.reason = reason;
+    await existing.save();
+  } else {
+    await UserRatings.create({ userId: user.id, system, score, reason });
+  }
+
+  await interaction.reply({ content: `✅ Rated <@${user.id}> **${score}/10** in **${system}**.`, ephemeral: true });
+};
