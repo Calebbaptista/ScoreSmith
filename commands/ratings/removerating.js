979ba8e86b9176887ab32222d113d9c13c54833e
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-09 21:03:05 -0400

    Update removerating.js

--- commands/ratings/removerating.js
+++ commands/ratings/removerating.js
@@ -1,19 +1,30 @@
-const UserRatings = require('../../models/UserRatings');
-const RatingSystem = require('../../models/RatingSystem');
+const { SlashCommandBuilder } = require('discord.js');
+const Rating = require('../../models/Rating');
 
-module.exports = async (interaction) => {
-  const user = interaction.options.getUser('user');
-  const system = interaction.options.getString('system');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('removerating')
+    .setDescription('Remove a rating from a user')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to remove rating from')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('type')
+        .setDescription('Rating type')
+        .setRequired(true)
+        .setAutocomplete(true)),
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    const type = interaction.options.getString('type');
+    const guildId = interaction.guild.id;
 
-  const systemExists = await RatingSystem.findOne({ name: system });
-  if (!systemExists) {
-    return interaction.reply({ content: '⚠️ Invalid rating system.', ephemeral: true });
+    const result = await Rating.findOneAndDelete({ userId: user.id, guildId, type });
+
+    if (result) {
+      await interaction.reply({ content: `🗑️ Removed one **${type}** rating from <@${user.id}>.`, flags: 64 });
+    } else {
+      await interaction.reply({ content: `⚠️ No rating of type **${type}** found for <@${user.id}>.`, flags: 64 });
+    }
   }
-
-  const deleted = await UserRatings.deleteOne({ userId: user.id, system });
-  if (deleted.deletedCount === 0) {
-    return interaction.reply({ content: `⚠️ <@${user.id}> has no rating in ${system}.`, ephemeral: true });
-  }
-
-  await interaction.reply({ content: `🗑️ Removed rating for <@${user.id}> in **${system}**.`, ephemeral: true });
 };
