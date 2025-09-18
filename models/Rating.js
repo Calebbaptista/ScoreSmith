97afe2ff5bc2acd1f2a08454644f60f7d6d3b6a8
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 05:41:34 -0400

    Update Rating.js

--- models/Rating.js
+++ models/Rating.js
@@ -1,24 +1,11 @@
 const mongoose = require('mongoose');
 
-const ratingSchema = new mongoose.Schema({
-  guildId: {
-    type: String,
-    required: true
-  },
-  userId: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: String,
-    required: true
-  },
-  givenBy: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
+const RatingSchema = new mongoose.Schema({
+  guildId:   { type: String, required: true },
+  userId:    { type: String, required: true },
+  rating:    { type: Number, required: true },
+  reason:    { type: String, required: true },
+  createdAt: { type: Date,   default: Date.now }
 });
 
-module.exports = mongoose.model('Rating', ratingSchema);
+module.exports = mongoose.model('Rating', RatingSchema);
