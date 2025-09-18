f7c916cb5a9bcc69b8b54f456b5757ddc348a508
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 03:18:09 -0400

    Update Point.js

--- models/Point.js
+++ models/Point.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-
 const pointSchema = new mongoose.Schema({
   userId: String,
   guildId: String,
   type: String,
   timestamp: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Point', pointSchema);
