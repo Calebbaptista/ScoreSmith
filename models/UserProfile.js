4de2ab64b4effc51c1049e3e881f7d4fc89e6880
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 04:42:21 -0400

    Update UserProfile.js

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
 
+const userProfileSchema = new mongoose.Schema({
+  guildId: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  points: {
+    type: Number,
+    default: 0
+  }
+}, {
+  timestamps: true
+});
+
+userProfileSchema.index({ guildId: 1, userId: 1 }, { unique: true });
+
+module.exports = mongoose.model('UserProfile', userProfileSchema);
