4c059c474d9e5dc2559a614500a4ffcafa817086
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 04:35:10 -0400

    Delete models/Point.js

--- models/Point.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const pointSchema = new mongoose.Schema({
-  userId: String,
-  guildId: String,
-  type: String,
-  timestamp: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Point', pointSchema);
