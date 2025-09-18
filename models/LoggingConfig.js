b81c66d038619230f6beec7d1e3739d9e45b7655
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 04:34:56 -0400

    Delete models/LoggingConfig.js

--- models/LoggingConfig.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const loggingConfigSchema = new mongoose.Schema({
-  guildId: String,
-  channelId: String
-});
-module.exports = mongoose.model('LoggingConfig', loggingConfigSchema);
