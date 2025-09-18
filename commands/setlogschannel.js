a76c67ed3837e1ec643d1e4ddacbbb483ca58ef9
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 04:45:29 -0400

    Update setlogschannel.js

--- commands/setlogschannel.js
+++ commands/setlogschannel.js
@@ -1,1 +1,33 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const GuildConfig = require('../../models/GuildConfig');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('setlogschannel')
+    .setDescription('Set the channel where logs will be sent')
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('The channel to send logs to')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const guildId = interaction.guild.id;
+    const channel = interaction.options.getChannel('channel');
+
+    // Save logs channel to GuildConfig
+    await GuildConfig.findOneAndUpdate(
+      { guildId },
+      { $set: { logsChannelId: channel.id } },
+      { upsert: true }
+    );
+
+    const embed = new EmbedBuilder()
+      .setTitle('📜 Logs Channel Set')
+      .setDescription(`Logs will now be sent to <#${channel.id}>.`)
+      .setColor(0x00BFFF)
+      .setFooter({ text: `Configured by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
