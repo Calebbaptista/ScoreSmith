7de69a5fd88833ff12a16b239c3082da1be92f12
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 04:41:33 -0400

    Update PointType.js

--- models/PointType.js
+++ models/PointType.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
+
 const pointTypeSchema = new mongoose.Schema({
-  guildId: String,
-  type: String
+  guildId: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
 });
+
+pointTypeSchema.index({ guildId: 1, type: 1 }, { unique: true });
+
 module.exports = mongoose.model('PointType', pointTypeSchema);
