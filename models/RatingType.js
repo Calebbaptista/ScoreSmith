4716cfd05a20386a5eb5f3cb110f5f2950e38587
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-18 04:35:58 -0400

    Delete models/RatingType.js

--- models/RatingType.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ratingTypeSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  name: { type: String, required: true }
-});
-
-module.exports = mongoose.model('RatingType', ratingTypeSchema);
