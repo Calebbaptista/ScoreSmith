961f79d45882cf9daf8e0e86df89aa026b7982d2
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-19 11:07:09 -0400

    Update addpoints.js

--- commands/addpoints.js
+++ commands/addpoints.js
@@ -1,4 +1,3 @@
-// commands/addpoints.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
 const Point = require('../models/Point');
@@ -10,37 +9,27 @@ module.exports = {
     .setName('addpoints')
     .setDescription('Add points of a given type to a user.')
     .addUserOption(option =>
-      option.setName('target')
-        .setDescription('Member to give points to')
-        .setRequired(true))
+      option.setName('target').setDescription('Member to give points to').setRequired(true))
     .addIntegerOption(option =>
-      option.setName('amount')
-        .setDescription('Number of points to add')
-        .setMinValue(1)
-        .setRequired(true))
+      option.setName('amount').setDescription('Number of points to add').setMinValue(1).setRequired(true))
     .addStringOption(option =>
-      option.setName('type')
-        .setDescription('Point type')
-        .setRequired(true)
-        .setAutocomplete(true)),
+      option.setName('type').setDescription('Point type').setRequired(true).setAutocomplete(true)),
 
   async autocomplete(interaction) {
     const focused = interaction.options.getFocused(true);
     if (focused.name !== 'type') return;
-
     const allTypes = await PointType.find({ guildId: interaction.guildId }).distinct('name');
     const choices = allTypes
       .filter(t => t.toLowerCase().startsWith(focused.value.toLowerCase()))
       .slice(0, 25)
       .map(t => ({ name: t, value: t }));
-
     await interaction.respond(choices);
   },
 
   async execute(interaction) {
-    const user   = interaction.options.getUser('target');
+    const user = interaction.options.getUser('target');
     const amount = interaction.options.getInteger('amount');
-    const type   = interaction.options.getString('type').toLowerCase();
+    const type = interaction.options.getString('type').toLowerCase();
 
     let record = await Point.findOneAndUpdate(
       { guildId: interaction.guildId, userId: user.id, type },
@@ -53,11 +42,11 @@ module.exports = {
       await record.save();
     }
 
-    await interaction.reply({
-      content: `➕ Added ${amount} **${type}** points to ${user.tag}. Total: ${record.amount}`,
-    });
+    await interaction.reply(
+      `➕ Added ${amount} **${type}** points to ${user.tag}. Total: ${record.amount}`
+    );
 
-    // 🔔 Log to guild's logs channel
+    // Log to guild's logs channel
     const config = await GuildConfig.findOne({ guildId: interaction.guildId });
     if (config?.logsChannelId) {
       const logChannel = interaction.guild.channels.cache.get(config.logsChannelId);
@@ -72,7 +61,6 @@ module.exports = {
             { name: 'New Total', value: `${record.amount}`, inline: true }
           )
           .setTimestamp();
-
         logChannel.send({ embeds: [embed] });
       }
     }
