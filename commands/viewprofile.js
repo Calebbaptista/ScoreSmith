0d65940f6e22fc3eff7be471e25110335a4c603d
Caleb <72480033+Calebbaptista@users.noreply.github.com>
2025-09-19 10:56:48 -0400

    Update viewprofile.js

--- commands/viewprofile.js
+++ commands/viewprofile.js
@@ -2,11 +2,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
 const Point = require('../models/Point');
+const PointType = require('../models/PointType');
+const Rating = require('../models/Rating');
+
+function getRatingLabel(ratings, amount) {
+  let chosen = 'Unrated';
+  for (const r of ratings.sort((a, b) => b.min - a.min)) {
+    if (amount >= r.min) {
+      chosen = r.label;
+      break;
+    }
+  }
+  return chosen;
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('viewprofile')
-    .setDescription('View a user’s point totals.')
+    .setDescription('View a user’s point totals and ratings.')
     .addUserOption(option =>
       option.setName('target')
         .setDescription('Member to view')
@@ -16,20 +29,12 @@ module.exports = {
   async execute(interaction) {
     const user = interaction.options.getUser('target') || interaction.user;
 
-    const points = await Point.find({ guildId: interaction.guildId, userId: user.id });
-
-    if (!points.length) {
-      return interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setTitle(`📊 Profile for ${user.tag}`)
-            .setDescription('This user has no points yet.')
-            .setColor(0x95a5a6)
-            .setThumbnail(user.displayAvatarURL())
-        ],
-        flags: 1 << 6
-      });
-    }
+    const validTypes = await PointType.find({ guildId: interaction.guildId }).distinct('name');
+    const points = await Point.find({
+      guildId: interaction.guildId,
+      userId: user.id,
+      type: { $in: validTypes }
+    });
 
     const embed = new EmbedBuilder()
       .setTitle(`📊 Profile for ${user.tag}`)
@@ -37,9 +42,20 @@ module.exports = {
       .setColor(0x3498db)
       .setTimestamp();
 
-    points.forEach(p => {
-      embed.addFields({ name: p.type, value: `${p.amount}`, inline: true });
-    });
+    if (!points.length) {
+      embed.setDescription('This user has no points yet.');
+    } else {
+      for (const p of points) {
+        const ratings = await Rating.find({ guildId: interaction.guildId, type: p.type });
+        const ratingLabel = getRatingLabel(ratings, p.amount);
+
+        embed.addFields({
+          name: `${p.type}`,
+          value: `${p.amount} points\n⭐ ${ratingLabel}`,
+          inline: true
+        });
+      }
+    }
 
     await interaction.reply({ embeds: [embed], flags: 1 << 6 });
   }
